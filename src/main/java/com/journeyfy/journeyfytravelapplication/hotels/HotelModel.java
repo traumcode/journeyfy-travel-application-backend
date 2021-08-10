c526fa4fe1c648d292624d5d1689991c078513c7
unknown <siminatabac@yahoo.com>
2021-08-10 18:48:57 +0300

    hotels' data for every city

--- src/main/java/com/journeyfy/journeyfytravelapplication/hotels/HotelModel.java
+++ src/main/java/com/journeyfy/journeyfytravelapplication/hotels/HotelModel.java
@@ -1,4 +1,21 @@
 package com.journeyfy.journeyfytravelapplication.hotels;
 
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class HotelModel {
+    private Long id;
+    private String name;
+    private double hotelClass;
+    private String picture;
+    private String description;
+    private double price;
+    private double rating;
+    private String cityName;
+    private String siteAddress;
 }
