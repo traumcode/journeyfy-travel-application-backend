47e1e70896e45bf9fc031000c2efe09d49ee4a09
FloareDeMai <matei.g.florentina@gmail.com>
2021-08-10 19:48:54 +0300

    added crossorigins

--- src/main/java/com/journeyfy/journeyfytravelapplication/museums/MuseumController.java
+++ src/main/java/com/journeyfy/journeyfytravelapplication/museums/MuseumController.java
@@ -1,16 +1,13 @@
 package com.journeyfy.journeyfytravelapplication.museums;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(path="museums")
 public class MuseumController {
