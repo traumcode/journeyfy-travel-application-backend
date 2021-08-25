2fe5f54c8fb35f1de9b62ca494d4349a359e1c5d
FloareDeMai <matei.g.florentina@gmail.com>
2021-08-25 14:37:20 +0300

    adde crud operations in museumController

--- src/main/java/com/journeyfy/journeyfytravelapplication/museums/MuseumController.java
+++ src/main/java/com/journeyfy/journeyfytravelapplication/museums/MuseumController.java
@@ -2,11 +2,14 @@ package com.journeyfy.journeyfytravelapplication.museums;
 
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -28,6 +31,39 @@ public class MuseumController {
 
     @PostMapping(path = "/add")
     public void addMuseum(@RequestBody Museum museum){
-        museumRepository.save(museum);
+        System.out.println(museumRepository.findByName(museum.getName()));
+        if(museumRepository.findByName(museum.getName()) != null){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "This hotel name already exist in database");
+        }else{
+            museumRepository.save(museum);
+        }
     }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public void deleteMuseum(@PathVariable("id") Long id){
+        museumRepository.delete(museumRepository.getById(id));
+    }
+
+    @PutMapping(path = "/edit/{id}")
+    public void editMuseumById(@PathVariable("id") Long id, @RequestBody Museum museum){
+        Optional<Museum> foundedMuseum = museumRepository.findById(id);
+        if(foundedMuseum.isPresent()){
+            if(museumRepository.findByName(museum.getName()) != null){
+                foundedMuseum.get().setPictureLink(museum.getPictureLink());
+                foundedMuseum.get().setName(museum.getName());
+                foundedMuseum.get().setDescription(museum.getDescription());
+                foundedMuseum.get().setRating(museum.getRating());
+                foundedMuseum.get().setPrice(museum.getPrice());
+                foundedMuseum.get().setCityName(museum.getCityName());
+                foundedMuseum.get().setSiteLink(museum.getSiteLink());
+                museumRepository.save(foundedMuseum.get());
+            }
+
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found");
+        }
+    }
+
+
+
 }
--- src/main/java/com/journeyfy/journeyfytravelapplication/museums/MuseumRepository.java
+++ src/main/java/com/journeyfy/journeyfytravelapplication/museums/MuseumRepository.java
@@ -1,11 +1,12 @@
 package com.journeyfy.journeyfytravelapplication.museums;
-
-import com.journeyfy.journeyfytravelapplication.hotels.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MuseumRepository extends JpaRepository<Museum, Long> {
     List<Museum> getMuseumsByCityName(String cityName);
     List<Museum> getMuseumsByRatingGreaterThan(double rating);
+    Museum findByName(String name);
 }
