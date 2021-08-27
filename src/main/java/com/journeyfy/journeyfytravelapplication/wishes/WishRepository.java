921f15c63ba29df7f15ea2e5c921299bad032a1c
FloareDeMai <matei.g.florentina@gmail.com>
2021-08-27 08:39:11 +0300

    changed wish controller and wish repo

--- src/main/java/com/journeyfy/journeyfytravelapplication/wishes/WishRepository.java
+++ src/main/java/com/journeyfy/journeyfytravelapplication/wishes/WishRepository.java
@@ -1,6 +1,7 @@
 package com.journeyfy.journeyfytravelapplication.wishes;
 
 
+import com.journeyfy.journeyfytravelapplication.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface WishRepository extends JpaRepository<Wish, Long> {
     Wish findWishByName(String name);
+    List<Wish> getAllByUser(User user);
 }
